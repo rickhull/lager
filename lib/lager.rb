76f69ce6cddea56791aa71161b628f67c674b2e4
Rick <rick@cloudscaling.com>
2013-07-04 21:24:46 -0700

    rake bump_patch

---
+++ lib/lager.rb
@@ -0,0 +1,40 @@
+require 'logger'
+
+module Lager
+  def self.log_to dest=nil
+    case dest
+    when nil, 'stderr', 'STDERR'
+      dest = $stderr
+    when 'stdout', 'STDOUT'
+      dest = $stdout
+    when IO
+      # do nothing
+    when String
+      # assume file path, do nothing
+    else
+      raise "unable to log_to #{dest} (#{dest.class})"
+    end
+    if defined?(@@log)
+      l = Logger.new dest
+      l.formatter = @@log.formatter
+      l.level = @@log.level
+      @@log = l
+    else
+      @@log = Logger.new dest
+      @@log.formatter = proc { |sev, time, progname, msg|
+        line = "[#{time.strftime('%Y-%m-%d %H:%M:%S')}] #{sev.to_s.upcase}: "
+        line << "(#{progname}) " if progname
+        line << msg << "\n"
+      }
+      @@log.level = Logger::WARN
+    end
+    @@log
+  end
+
+  def self.log_level=(sym)
+    log_to unless defined?(@@log)
+    level = Logger.const_get(sym.to_s.upcase)
+    raise "unknown log level #{sym}" unless level
+    @@log.level = level
+  end
+end
