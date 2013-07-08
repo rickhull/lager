b4a30a1cd2245b04369958dbce88a407246fd760
Rick <rick@cloudscaling.com>
2013-07-07 21:32:51 -0700

    use_gemspec_file = true

--- rakefile.rb
+++ rakefile.rb
@@ -5,19 +5,8 @@ Buildar.conf(__FILE__) do |b|
   b.name = 'lager'
   b.use_version_file  = true
   b.version_filename  = 'VERSION'
-  b.use_manifest_file = true
-  b.manifest_filename = 'MANIFEST.txt'
   b.use_git           = true
   b.publish[:rubygems] =  true
-
-  b.gemspec.summary     = "Sane class-level logging"
-  b.gemspec.description = "Should you use it? Yes."
-  b.gemspec.author      = "Rick Hull"
-  b.gemspec.homepage    = "http://github.com/rickhull/lager"
-  b.gemspec.license     = 'LGPL'
-
-  b.gemspec.add_development_dependency "minitest", [">= 0"]
-  b.gemspec.add_development_dependency  "buildar", ["~> 1.2"]
 end
 
 Rake::TestTask.new :test do |t|
