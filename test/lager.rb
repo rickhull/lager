f36dc2638f2e6b0e1cf19993091cfb3966ceac43
Rick Hull <rick.hull@gmail.com>
2022-05-02 21:23:28 -0400

    add expect() wrappers for MiniTest 6

--- test/lager.rb
+++ test/lager.rb
@@ -1,7 +1,6 @@
-require 'minitest/autorun'
 require 'tempfile'
-
 require 'lager'
+require 'minitest/autorun'
 
 # useful Foo class
 # calls log_to at require time
@@ -10,28 +9,28 @@ require_relative '../examples/foo'
 
 describe Lager do
   describe ".version" do
-    it "must return a string of numbers and dots" do
-      Lager.version.must_match %r{\A[0-9\.]+\z}
+    it "returns a string of numbers and dots" do
+      expect(Lager.version).must_match %r{\A[0-9\.]+\z}
     end
   end
 
   describe "#log_to" do
     it "must have created a Logger" do
       # note, the useful Foo class has already called log_to
-      Foo.lager.must_be_instance_of(Logger)
+      expect(Foo.lager).must_be_instance_of(Logger)
     end
 
     it "must return its argument" do
-      Foo.log_to($stdout).must_equal $stdout
+      expect(Foo.log_to($stdout)).must_equal $stdout
     end
 
     it "must use an existing Logger when provided" do
       l = Logger.new($stderr)
       Foo.log_to l
-      Foo.lager.must_equal l
+      expect(Foo.lager).must_equal l
       Foo.log_level = :info
       Foo.bar # does debug logging, should be silent
-      Foo.lager.must_equal l
+      expect(Foo.lager).must_equal l
     end
 
     it "must handle a Tempfile when provided" do
@@ -40,7 +39,7 @@ describe Lager do
       Foo.log_level = :debug
       Foo.bar # does debug logging
       t.rewind
-      t.read.wont_be_empty
+      expect(t.read).wont_be_empty
       t.close
       t.unlink
     end
@@ -51,7 +50,7 @@ describe Lager do
       Foo.log_level = :debug
       Foo.bar # does debug logging
       Foo.log_to $stderr
-      File.exist?(fname).must_equal true
+      expect(File.exist?(fname)).must_equal true
       File.unlink fname
     end
   end
@@ -63,17 +62,17 @@ describe Lager do
 
     it "must accept :debug as :debug" do
       Foo.log_level = :debug
-      Foo.log_level.must_equal :debug
+      expect(Foo.log_level).must_equal :debug
     end
 
     it "must accept Logger::INFO as :info" do
       Foo.log_level = Logger::INFO
-      Foo.log_level.must_equal :info
+      expect(Foo.log_level).must_equal :info
     end
 
     it "must accept 'warn' as :warn" do
       Foo.log_level = 'warn'
-      Foo.log_level.must_equal :warn
+      expect(Foo.log_level).must_equal :warn
     end
 
     def must_ignore level
@@ -81,7 +80,7 @@ describe Lager do
       capture_subprocess_io do # suppress warnings
         Foo.log_level = level
       end
-      Foo.log_level.must_equal lev
+      expect(Foo.log_level).must_equal lev
     end
 
     it "must ignore a bad symbol" do
